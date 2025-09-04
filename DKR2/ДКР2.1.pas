9ef4c71ec0af46cebc78abf4fbe6926fe9822669
MACANtut <bestgirlintheworld23@gmail.com>
2025-09-05 00:39:08 +0300

    ()

---
+++ DKR2/ДКР2.1.pas
@@ -0,0 +1,35 @@
+﻿var
+  s, k, sum_k, j: Integer;
+  my_mass: array of Integer;
+
+begin
+  Write('Введите любое число: ');
+  ReadLn(s);
+  
+  Write('Введите количество элементов массива: ');
+  ReadLn(k);
+  SetLength(my_mass, k);
+  
+  Write('Ввод элементов массива: ');
+  for j := 0 to k - 1 do
+    Read(my_mass[j]);
+  
+  k := 0;
+  sum_k := 0;
+  j := 0;
+
+  while sum_k <= s do
+  begin
+    if j < Length(my_mass) then
+    begin
+      sum_k := sum_k + my_mass[j];
+      k += 1;
+      j += 1;
+    end
+    else
+      Break; 
+  end;
+
+  WriteLn(k - 1, '-количество первый k чиссел в массиве');
+  WriteLn(sum_k - my_mass[k - 1], '-сумма первых k чисел в массиве');
+end.
---
+++ DKR2/ДКР2.2.pas
@@ -0,0 +1,36 @@
+﻿var
+  myString: string;
+  i, letterCount, wordCount: Integer;
+  inWord: Boolean;
+
+begin
+  letterCount := 0;
+  wordCount := 0;
+  inWord := False;
+
+  Write('Введите строку: ');
+  ReadLn(myString);
+
+  for i := 1 to Length(myString) do
+  begin
+    if (myString[i] >= 'A') and (myString[i] <= 'Z') or
+       (myString[i] >= 'a') and (myString[i] <= 'z') or
+       (myString[i] >= 'А') and (myString[i] <= 'Я') or
+       (myString[i] >= 'а') and (myString[i] <= 'я') then
+    begin
+      letterCount += 1; 
+      if not inWord then
+      begin
+        inWord := True; // Начало слова
+        wordCount += 1; 
+      end;
+    end
+    else
+    begin
+      inWord := False; // Конец слова
+    end;
+  end;
+
+  WriteLn('Количество букв: ', letterCount);
+  WriteLn('Количество слов: ', wordCount);
+end.
